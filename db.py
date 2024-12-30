2a1daf6ea20c19bc63a5263c23a64eaf721e973b
Shamer27 <sam.hamer@education.nsw.gov.au>
2024-12-30 15:27:41 +1100

    Linking Pages
    Added html to all pages so users can access them all. Currently only for testing purposes and not all pages have correct funtionality

--- db.py
+++ db.py
@@ -16,3 +16,50 @@ def GetAllReviews():
     reviews = db.execute("SELECT * FROM Reviews").fetchall()
     db.close()
     return reviews
+
+def CheckLogin(username, password):
+
+    db = GetDB()
+
+    # Ask the database for a single user matching the provided name
+    user = db.execute("SELECT * FROM Users WHERE username=?", (username,)).fetchone()
+
+    # Do they exist?
+    if user is not None:
+        # OK they exist, is their password correct
+        if check_password_hash(user['password'], password):
+            # They got it right, return their details
+            return user
+       
+    # If we get here, the username or password failed.
+    return None
+
+
+
+def RegisterUser(username, password):
+
+    # Check if they gave us a username and password
+    if username is None or password is None:
+        return False
+
+    # Attempt to add them to the database
+    db = GetDB()
+    hash = generate_password_hash(password)
+    db.execute("INSERT INTO Users(username, password) VALUES(?, ?)", (username, hash,))
+    db.commit()
+
+    return True
+
+def AddGuess(user_id, title, review, date, game, score):
+   
+    # Check if any boxes were empty
+    if date is None or game is None:
+        return False
+   
+    # Get the DB and add the guess
+    db = GetDB()
+    db.execute("INSERT INTO Guesses(user_id, date, game, score) VALUES (?, ?, ?, ?)",
+               (user_id, date, game, score, review, title))
+    db.commit()
+
+    return True
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 import db
 
 app = Flask(__name__)
@@ -8,5 +8,41 @@ app.secret_key = "gtg"
 def Home():
     reviewData = db.GetAllReviews()
     return render_template("index.html", reviews=reviewData)
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def Register():
+
+    # If they click the submit button, let's register
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Try and add them to the DB
+        if db.RegisterUser(username, password):
+            # Success! Let's go to the homepage
+            return redirect("/")
+        
+    return render_template("register.html")
+
+@app.route("/createReview", methods=["GET","POST"])
+def createReview():
+
+    # Did they click submit?
+    if request.method == "POST":
+        user_id = session['id']
+        title = request.form['title']
+        review = request.form['review']
+        date = request.form['date']
+        game = request.form['game']
+        score = request.form['score']
+
+        # Send the data to add our new guess to the db
+        db.AddGuess(user_id, date, game, score)
+
+    return render_template("createReview.html")
 
 app.run(debug=True, port=5000)
